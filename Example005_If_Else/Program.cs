6ca044e8704b834a930b50b1eb4887efb65e1431
Nadezhda <S_Nadja@bk.ru>
2022-07-09 12:31:20 +0300

    Example005 added

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string unsername = Console.ReadLine();
+
+if(unsername.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(unsername);
+}
